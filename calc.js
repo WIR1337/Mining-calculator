4c5ba73460e5abde93e1a2730eaf5713c43e0a9c
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-17 19:55:22 +0500

    edit variable names

--- calc.js
+++ calc.js
@@ -1,12 +1,23 @@
 const hoursInMonths = 720
 const kwPrice = 3.4
 
-var powerUsage = (72 * 12 + 100) / 1000
+var cardWtUsage = 72
+var cardNumber = 12
+var rigWtUsage = 80
 
-let totalWt = hoursInMonths * powerUsage.toFixed(1)
+var powerUsagePerHour = (cardWtUsage * cardNumber + rigWtUsage) / 1000
 
-let sumPerMonths = totalWt * kwPrice
+let powerUsagePerMonth = hoursInMonths * powerUsagePerHour.toFixed(1)
 
-console.log(`For 12 cards ${totalWt}kWt/h`)
+let sumPerMonths = powerUsagePerMonth * kwPrice
+
+console.log(`For 12 cards ${powerUsagePerMonth}kWt/h`)
 console.log(`Spend for electricity per month ${sumPerMonths} RUB`)
-console.log(`Spend for electricity per day ${sumPerMonths/30} RUB`)
+console.log(`Spend for electricity per day ${sumPerMonths / 30} RUB`)
+
+
+function miningCalc() {
+    const hoursInMonths = 720
+    const kWPriceRUB = 3.4
+    const kWPriceUSD = 0.05
+}
