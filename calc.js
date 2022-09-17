97dbe8eb32e479ac0d151425bccbe50aa2517c1f
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-17 17:54:40 +0500

    add basic function

--- calc.js
+++ calc.js
@@ -1,1 +1,12 @@
 const hoursInMonths = 720
+const kwPrice = 3.4
+
+var powerUsage = (72 * 12 + 100) / 1000
+
+let totalWt = hoursInMonths * powerUsage.toFixed(1)
+
+let sumPerMonths = totalWt * kwPrice
+
+console.log(`For 12 cards ${totalWt}kWt/h`)
+console.log(`Spend for electricity per month ${sumPerMonths} RUB`)
+console.log(`Spend for electricity per day ${sumPerMonths/30} RUB`)
