17f404e8889e2fecab4e304cfd24989862915b52
Alexander Naberezhnyi <naberezhnyy.an@gmail.com>
2022-09-17 20:35:42 +0500

    create miningCalc

--- calc.js
+++ calc.js
@@ -1,23 +1,36 @@
-const hoursInMonths = 720
-const kwPrice = 3.4
-
-var cardWtUsage = 72
-var cardNumber = 12
-var rigWtUsage = 80
-
-var powerUsagePerHour = (cardWtUsage * cardNumber + rigWtUsage) / 1000
-
-let powerUsagePerMonth = hoursInMonths * powerUsagePerHour.toFixed(1)
-
-let sumPerMonths = powerUsagePerMonth * kwPrice
-
-console.log(`For 12 cards ${powerUsagePerMonth}kWt/h`)
-console.log(`Spend for electricity per month ${sumPerMonths} RUB`)
-console.log(`Spend for electricity per day ${sumPerMonths / 30} RUB`)
-
-
-function miningCalc() {
+console.log('--------------------------------')
+function miningCalc(cardWtUsage, cardNumber, rigWtUsage, usdPrice) {
     const hoursInMonths = 720
     const kWPriceRUB = 3.4
-    const kWPriceUSD = 0.05
+
+    var powerUsagePerHourKWT = (cardWtUsage * cardNumber + rigWtUsage) / 1000
+    var powerUsagePerMonthKWT = hoursInMonths * powerUsagePerHourKWT.toFixed()
+
+    var sumPerMonthsRUB = powerUsagePerMonthKWT * kWPriceRUB
+    var sumPerMonthsUSD = powerUsagePerMonthKWT * kWPriceRUB / usdPrice
+
+    // Russian edition
+    // console.log(`При стоимости 1kWth/h = ${kWPriceRUB} RUB и курса 1$ = ${usdPrice} RUB`)
+    // console.log(`Расход за час ${powerUsagePerHourKWT.toFixed()} kWt/h`)
+    // console.log(`Расход за мес ${powerUsagePerMonthKWT.toFixed()} kWt/h`)
+
+    // console.log(`Расход в сутки ${(sumPerMonthsRUB / 30).toFixed(2)} RUB`)
+    // console.log(`Расход в сутки ${(sumPerMonthsUSD / 30).toFixed(2)} USD`)
+
+    // console.log(`Расходы в мес ${sumPerMonthsRUB.toFixed(2)} RUB`)
+    // console.log(`Расходы в мес ${sumPerMonthsUSD.toFixed(2)} USD`)
+
+
+    console.log(`At a cost 1kWth/h = ${kWPriceRUB} RUB and course 1$ = ${usdPrice} RUB`)
+    console.log(`Consumption per hour ${powerUsagePerHourKWT.toFixed()} kWt/h`)
+    console.log(`Consumption per month ${powerUsagePerMonthKWT.toFixed()} kWt/h`)
+
+    console.log(`Consumption per day ${(sumPerMonthsRUB / 30).toFixed(2)} RUB`)
+    console.log(`Consumption per day ${(sumPerMonthsUSD / 30).toFixed(2)} USD`)
+
+    console.log(`Consumption per month ${sumPerMonthsRUB.toFixed(2)} RUB`)
+    console.log(`Consumption per month ${sumPerMonthsUSD.toFixed(2)} USD`)
 }
+
+miningCalc(72, 12, 80, 62)
+console.log('--------------------------------')
